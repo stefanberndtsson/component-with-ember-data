b4660a0cb39646b5ae25ab81171fb78c107688d1
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-17 22:03:29 +0200

    Expose integer id from DS.Model string id

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,3 +3,10 @@ import DS from "ember-data";
 export default DS.ActiveModelAdapter.extend({
     host: "http://localhost:3019"
 });
+
+DS.Model.reopen({
+    idInt: function() {
+	if(!this.get('id')) { return undefined; }
+	return parseInt(this.get('id'));
+    }.property('id')
+});
