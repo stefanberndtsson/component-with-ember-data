bf87b0f31816cba0f14711840c1a38b2da461951
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-25 21:45:26 +0200

    Add query count and such

--- app/controllers/components/index.js
+++ app/controllers/components/index.js
@@ -16,5 +16,27 @@ export default Ember.ArrayController.extend({
 	    pArray.push(p);
 	}
 	return Ember.ArrayProxy.create({content: Ember.A(pArray)});
-    }.property('model.meta.pagination.pages', 'model.meta.pagination.page')
+    }.property('model.meta.pagination.pages', 'model.meta.pagination.page'),
+    pageStart: function() {
+	var currentPage = this.get('model.meta.pagination.page');
+	var perPage = this.get('model.meta.pagination.per_page');
+	if(!perPage) { return 0; }
+	return ((currentPage-1) * perPage)+1;
+    }.property('model.meta.pagination.page', 'model.meta.pagination.per_page'),
+    pageEnd: function() {
+	var currentPage = this.get('model.meta.pagination.page');
+	var perPage = this.get('model.meta.pagination.per_page');
+	var total = this.get('model.meta.query.total');
+	if(!perPage) { return 0; }
+	var pageEnd = ((currentPage-1) * perPage)+perPage;
+	if(pageEnd > total) {
+	    return total;
+	} else {
+	    return pageEnd;
+	}
+    }.property('model.meta.pagination.page', 'model.meta.pagination.per_page', 'model.meta.query.total'),
+    singleResult: function() {
+	if(this.get('model.meta.query.total') == 1) { return true; }
+	return false;
+    }.property('model.meta.query.total')
 });
