1854f02193f5f3025e538aa901f2ca3efe778f97
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-26 16:30:28 +0100

    Revert lint fix and fix it in jshintrc instead

--- app/helpers/markdown-text.js
+++ app/helpers/markdown-text.js
@@ -1,5 +1,4 @@
 import Ember from "ember";
-import markdown from "ember";
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
     return new Ember.Handlebars.SafeString(markdown.toHTML(value));
