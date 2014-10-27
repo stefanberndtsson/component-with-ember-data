1a1ca62109438fdeb94443abcacf6cdae7a5779e
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-27 22:39:42 +0100

    Use CSS to scale image, so that Firefox actually does it usefully

--- app/helpers/pdf-link.js
+++ app/helpers/pdf-link.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Handlebars.makeBoundHelper(function(id){
-    var img = "<img src=\"/img/pdf-file-128.png\" height=\"24\"/>";
+    var img = "<img class=\"pdf-image\" src=\"/img/pdf-file-128.png\"/>";
     var str = "<a target=\"_blank\" href=\""+ENV.APP.fileURL+"/"+id+"\">"+img+"</a>";
     return new Ember.Handlebars.SafeString(str);
 });
