471442c712337cee1a31f72395a8db37e387ad4a
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-10-28 21:06:43 +0100

    First try of responsiveness working on mobile

--- app/helpers/image-link.js
+++ app/helpers/image-link.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 
 export default Ember.Handlebars.makeBoundHelper(function(id, name, size){
-    var img = "<img src=\""+ENV.APP.fileURL+"/thumbnail/"+id+"?size="+size+"\"/>";
+    var img = "<img class=\"image-link-thumbnail\" src=\""+ENV.APP.fileURL+"/thumbnail/"+id+"?size="+size+"\"/>";
     var str = "<a target=\"_blank\" href=\""+ENV.APP.fileURL+"/"+id+"\">"+img+"</a>";
     return new Ember.Handlebars.SafeString(str);
 });
