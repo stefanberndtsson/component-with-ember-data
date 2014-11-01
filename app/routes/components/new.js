d8012d4ae7fed08a2bb455061fdf38ef293e1744
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-01 16:28:49 +0100

    Reduce code duplication further by inheriting from edit route

--- app/routes/components/new.js
+++ app/routes/components/new.js
@@ -1,30 +1,7 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import ComponentsEditRoute from '../components/edit';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default ComponentsEditRoute.extend({
     model: function() {
 	return this.store.createRecord('component', {});
-    },
-    setupController: function(controller, model) {
-	controller.set('model', model);
-	controller.set('error', false);
-    },
-    actions: {
-	saveComponent: function(component) {
-	    var that = this;
-	    component.save().then(function(newModel) {
-		newModel.unloadRecord();
-		that.store.find('tag').then(function(tags) {
-		    that.controllerFor('application').set('tagsSelection', tags);
-		});
-		that.transitionTo('components.show', newModel.id);
-	    },function(reason) {
-		if(reason.status === 401) {
-		    that.send('invalidateSession');
-		} else {
-		    that.controller.set('error', true);
-		}
-	    });
-	}
     }
 });
