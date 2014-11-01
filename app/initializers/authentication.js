49aae98aae32acf03efdb827d1c26a37e2bd96c2
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2014-11-01 21:50:34 +0100

    Stop 5 second auto-refresh of session and just rely on the backend to extend valid time on every request

--- app/initializers/authentication.js
+++ app/initializers/authentication.js
@@ -3,44 +3,19 @@ import Base from 'simple-auth/authenticators/base';
 import ENV from '../config/environment';
 
 var CustomAuthenticator = Base.extend({
-    refreshIdentifier: null,
-    refreshSession: function(token) {
-	var that = this;
-	Ember.$.ajax({
-	    type: 'GET',
-	    url: ENV.APP.authenticationBaseURL+'/'+token
-	}).then(function(response) {
-	    Ember.run(function() {
-		var session = that.container.lookup('simple-auth-session:main');
-		session.set('username', response.user.username);
-		session.set('name', response.user.name);
-	    });
-	}, function() {
-	    var controller = that.container.lookup('controller:application');
-	    controller.send('invalidateSession');
-	});
-	this.refreshIdentifier = Ember.run.later(function() {
-	    that.refreshSession(token);
-	}, 5000);
-    },
     restore: function(properties) {
-	var that = this;
 	return new Ember.RSVP.Promise(function(resolve, reject) {
 	    Ember.$.ajax({
 		type: 'GET',
 		url: ENV.APP.authenticationBaseURL+'/'+properties.token
 	    }).then(function() {
 		resolve(properties);
-		that.refreshIdentifier = Ember.run.later(function() {
-		    that.refreshSession(properties.token);
-		}, 5000);
 	    }, function() {
 		reject();
 	    });
 	});
     },
     authenticate: function(credentials) {
-	var that = this;
 	return new Ember.RSVP.Promise(function(resolve, reject) {
 	    Ember.$.ajax({
 		type: 'POST',
@@ -60,9 +35,6 @@ var CustomAuthenticator = Base.extend({
 			name: response.user.name
 		    });
 		});
-		that.refreshIdentifier = Ember.run.later(function() {
-		    that.refreshSession(token);
-		}, 5000);
 	    }, function(xhr, status, error) {
 		Ember.run(function() {
 		    reject(error);
@@ -71,10 +43,6 @@ var CustomAuthenticator = Base.extend({
 	});
     },
     invalidate: function() {
-	if(this.refreshIdentifier) {
-	    Ember.run.cancel(this.refreshIdentifier);
-	    this.refreshIdentifier = null;
-	}
 	return new Ember.RSVP.Promise(function(resolve) {
 	    resolve();
 	});
